97d89008cd048ad3756c1e4e861996aafc5b69b5
Ahmet Furkan KISACIK <ahmetfurkankisacik@gmail.com>
2024-09-05 00:26:55 +0300

    "School Managment System All Documents"

---
+++ src/main/java/com/project/controller/user/StudentController.java
@@ -0,0 +1,67 @@
+package com.project.controller.user;
+
+import com.project.payload.request.business.ChooseLessonProgramWithId;
+import com.project.payload.request.user.StudentRequest;
+import com.project.payload.request.user.StudentRequestWithoutPassword;
+import com.project.payload.response.business.ResponseMessage;
+import com.project.payload.response.user.StudentResponse;
+import com.project.entity.contactmessage.entity.service.user.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/student")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @PostMapping("/save") // http://localhost:8080/student/save + JSON + POST
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public ResponseEntity<ResponseMessage<StudentResponse>> saveStudent(
+            @RequestBody @Valid StudentRequest studentRequest){
+        return ResponseEntity.ok(studentService.saveStudent(studentRequest));
+    }
+
+    // Not: updateStudentForStudents() **********************************************************
+    // !!! ogrencinin kendisini update etme islemi
+    @PatchMapping("/update")   // http://localhost:8080/student/updateStudent
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
+    public ResponseEntity<String> updateStudent(@RequestBody @Valid
+                                                    StudentRequestWithoutPassword studentRequestWithoutPassword,
+                                                    HttpServletRequest request){
+        return studentService.updateStudent(studentRequestWithoutPassword, request);
+    }
+
+    // Not: updateStudent() **********************************************************
+    @PutMapping("/update/{userId}")   // http://localhost:8080/student/update/2
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
+    public ResponseMessage<StudentResponse>updateStudentForManagers(
+            @PathVariable Long userId,
+            @RequestBody @Valid StudentRequest studentRequest){
+        return studentService.updateStudentForManagers(userId,studentRequest);
+    }
+    // Not: ChangeActıveStatusOfStudent() ***********************************************
+    @PreAuthorize("hasAnyAuthority('ADMIN','MANAGER','ASSISTANT_MANAGER')")
+    @GetMapping("/changeStatus") // http://localhost:8080/student/changeStatus?id=1&status=true
+    public ResponseMessage changeStatusOfStudent (@RequestParam Long id, @RequestParam boolean status){
+        return studentService.changeStatusOfStudent(id,status);
+    }
+
+    // !!! Student kendine lessonProgram ekliyor
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
+    @PostMapping("/addLessonProgramToStudent") // http://localhost:8080/student/addLessonProgramToStudent
+    public ResponseMessage<StudentResponse> addLessonProgram(HttpServletRequest request,
+                                                   @RequestBody @Valid ChooseLessonProgramWithId chooseLessonProgramWithId){
+        String userName = (String) request.getAttribute("username");
+
+        return studentService.addLessonProgramToStudent(userName, chooseLessonProgramWithId);
+    }
+
+
+}
---
+++ src/main/java/com/project/entity/contactmessage/entity/service/user/TeacherService.java
@@ -0,0 +1,186 @@
+package com.project.entity.contactmessage.entity.service.user;
+
+import com.project.entity.concretes.business.LessonProgram;
+import com.project.entity.concretes.user.User;
+import com.project.entity.contactmessage.entity.service.helper.MethodHelper;
+import com.project.entity.enums.RoleType;
+import com.project.exception.ConflictException;
+import com.project.payload.mappers.UserMapper;
+import com.project.payload.messages.ErrorMessages;
+import com.project.payload.messages.SuccessMessages;
+import com.project.payload.request.business.ChooseLessonTeacherRequest;
+import com.project.payload.request.user.TeacherRequest;
+import com.project.payload.response.business.ResponseMessage;
+import com.project.payload.response.user.StudentResponse;
+import com.project.payload.response.user.TeacherResponse;
+import com.project.payload.response.user.UserResponse;
+import com.project.repository.user.UserRepository;
+import com.project.entity.contactmessage.entity.service.business.LessonProgramService;
+import com.project.entity.contactmessage.entity.service.validator.DateTimeValidator;
+import com.project.entity.contactmessage.entity.service.validator.UniquePropertyValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class TeacherService {
+
+    private final UserRepository userRepository;
+    private final UniquePropertyValidator uniquePropertyValidator;
+    private final UserMapper userMapper;
+    private final UserRoleService userRoleService;
+    private final PasswordEncoder passwordEncoder;
+    private final MethodHelper methodHelper;
+    private final LessonProgramService lessonProgramService;
+    private final DateTimeValidator dateTimeValidator;
+
+    public ResponseMessage<TeacherResponse> saveTeacher(TeacherRequest teacherRequest) {
+
+        Set<LessonProgram> lessonProgramSet =
+                lessonProgramService.getLessonProgramById(teacherRequest.getLessonIdList());
+
+        //!!! unique kontrolu
+        uniquePropertyValidator.checkDuplicate(teacherRequest.getUsername(),
+                teacherRequest.getSsn(), teacherRequest.getPhoneNumber(),teacherRequest.getEmail());
+
+        // DTO --> POJO
+        User teacher = userMapper.mapTeacherRequestToUser(teacherRequest);
+
+        teacher.setUserRole(userRoleService.getUserRole(RoleType.TEACHER));
+        teacher.setLessonsProgramList(lessonProgramSet);
+        teacher.setPassword(passwordEncoder.encode(teacher.getPassword()));
+        if(teacherRequest.getIsAdvisorTeacher()){
+            teacher.setIsAdvisor(Boolean.TRUE);
+        } else teacher.setIsAdvisor(Boolean.FALSE);
+
+        User savedTeacher = userRepository.save(teacher);
+
+        return ResponseMessage.<TeacherResponse>builder()
+                .message(SuccessMessages.TEACHER_SAVE)
+                .httpStatus(HttpStatus.CREATED)
+                .object(userMapper.mapUserToTeacherResponse(savedTeacher))
+                .build();
+    }
+
+    public List<StudentResponse> getAllStudentByAdvisorUsername(String userName) {
+
+        User teacher = methodHelper.isUserExistByUsername(userName);
+
+        methodHelper.checkAdvisor(teacher);
+
+        return userRepository.findByAdvisorTeacherId(teacher.getId())
+                .stream()
+                .map(userMapper::mapUserToStudentResponse)
+                .collect(Collectors.toList());
+
+    }
+
+    public List<UserResponse> getAllAdvisorTeacher() {
+
+        return userRepository.findAllByAdvisor(Boolean.TRUE)
+                .stream()
+                .map(userMapper::mapUserToUserResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ResponseMessage<TeacherResponse> updateTeacherForManagers(TeacherRequest teacherRequest, Long userId) {
+
+        User user = methodHelper.isUserExist(userId);
+        // !!! Parametrede gelen id bir teacher a ait degilse exception firlatiliyor
+        methodHelper.checkRole(user,RoleType.TEACHER);
+
+        Set<LessonProgram> lessonProgramSet =
+                lessonProgramService.getLessonProgramById(teacherRequest.getLessonIdList());
+
+        // !!! unique kontrolu
+        uniquePropertyValidator.checkUniqueProperties(user, teacherRequest);
+        // !!! DTO --> POJO
+        User updatedTeacher = userMapper.mapTeacherRequestToUpdatedUser(teacherRequest, userId);
+        // !!! props. that does n't exist in mappers
+        updatedTeacher.setPassword(passwordEncoder.encode(teacherRequest.getPassword()));
+        updatedTeacher.setLessonsProgramList(lessonProgramSet);
+        updatedTeacher.setUserRole(userRoleService.getUserRole(RoleType.TEACHER));
+
+        User savedTeacher = userRepository.save(updatedTeacher);
+
+        return ResponseMessage.<TeacherResponse>builder()
+                .object(userMapper.mapUserToTeacherResponse(savedTeacher))
+                .message(SuccessMessages.TEACHER_UPDATE)
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
+
+    public ResponseMessage<UserResponse> saveAdvisorTeacher(Long teacherId) {
+
+        // !!! Save de yazdigimiz ya varsa kontrolu
+        User teacher = methodHelper.isUserExist(teacherId);
+        // !!! id ile gelen uer Teacher mi kontrolu
+        methodHelper.checkRole(teacher,RoleType.TEACHER);
+
+        // !!! id ile gelen teacher zaten advisor mi kontrolu ?
+        if(Boolean.TRUE.equals(teacher.getIsAdvisor())) { // condition : teacher.getIsAdvisor()
+            throw new ConflictException(
+                    String.format(ErrorMessages.ALREADY_EXIST_ADVISOR_MESSAGE, teacherId));
+        }
+
+        teacher.setIsAdvisor(Boolean.TRUE);
+        userRepository.save(teacher);
+
+        return ResponseMessage.<UserResponse>builder()
+                .message(SuccessMessages.ADVISOR_TEACHER_SAVE)
+                .object(userMapper.mapUserToUserResponse(teacher))
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
+
+    public ResponseMessage<UserResponse> deleteAdvisorTeacherById(Long teacherId) {
+        User teacher = methodHelper.isUserExist(teacherId);
+        // !!! id ile gelen user Teacher mi kontrolu
+        methodHelper.checkRole(teacher,RoleType.TEACHER);
+
+        // !!! id ile gelen teacheradvisor mi kontrolu ?
+        methodHelper.checkAdvisor(teacher);
+
+        teacher.setIsAdvisor(Boolean.FALSE);
+        userRepository.save(teacher);
+
+        // !!! silinen advisor Teacherlarin Student lari varsa bu iliskinin de koparilmasi gerekiyor
+        List<User> allStudents = userRepository.findByAdvisorTeacherId(teacherId);
+        if(!allStudents.isEmpty()) {
+            allStudents.forEach(students -> students.setAdvisorTeacherId(null));
+        }
+
+        return ResponseMessage.<UserResponse>builder()
+                .message(SuccessMessages.ADVISOR_TEACHER_DELETE)
+                .object(userMapper.mapUserToUserResponse(teacher))
+                .httpStatus(HttpStatus.OK)
+                .build();
+    }
+
+    public ResponseMessage<TeacherResponse> addLessonProgram(ChooseLessonTeacherRequest chooseLessonTeacherRequest) {
+
+        User teacher = methodHelper.isUserExist(chooseLessonTeacherRequest.getTeacherId());
+        methodHelper.checkRole(teacher, RoleType.TEACHER);
+
+        Set<LessonProgram> lessonPrograms =
+                lessonProgramService.getLessonProgramById(chooseLessonTeacherRequest.getLessonProgramId());
+        Set<LessonProgram> teachersLessonProgram = teacher.getLessonsProgramList();
+        // conflict kontrolu
+        dateTimeValidator.checkLessonPrograms(teachersLessonProgram, lessonPrograms);
+        teachersLessonProgram.addAll(lessonPrograms);
+        teacher.setLessonsProgramList(teachersLessonProgram);
+        User savedTeacher = userRepository.save(teacher);
+
+        return ResponseMessage.<TeacherResponse>builder()
+                .message(SuccessMessages.LESSON_PROGRAM_ADD_TO_TEACHER)
+                .httpStatus(HttpStatus.OK)
+                .object(userMapper.mapUserToTeacherResponse(savedTeacher))
+                .build();
+    }
+}
